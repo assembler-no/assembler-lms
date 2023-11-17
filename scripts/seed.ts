d32619af48dc27dc188fbfb38d71141eb8541742
Daniel Stelea <daniel@digitnord.no>
2023-11-17 03:22:37 +0200

    Fix

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,24 +1,16 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 
 const database = new PrismaClient();
 
 async function main() {
   try {
     await database.category.createMany({
-      data: [
-        { name: "Computer Science" },
-        { name: "Music" },
-        { name: "Fitness" },
-        { name: "Photography" },
-        { name: "Accounting" },
-        { name: "Engineering" },
-        { name: "Filming" },
-      ]
+      data: [{ name: 'AI' }]
     });
 
-    console.log("Success");
+    console.log('Success');
   } catch (error) {
-    console.log("Error seeding the database categories", error);
+    console.log('Error seeding the database categories', error);
   } finally {
     await database.$disconnect();
   }
